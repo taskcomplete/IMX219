68787c9f446ff350dc70e8a76c43014d39aaa7b1
taskcomplete <52295038+taskcomplete@users.noreply.github.com>
2025-01-02 22:51:43 +0200

    Add generate_frames

--- raspberry-stream.py
+++ raspberry-stream.py
@@ -1,1 +1,27 @@
+import cv2
+from flask import Flask, Response
 
+app = Flask(__name__)
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)  # 0 - стандартна камера
+
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            # Кодування кадру в JPEG
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            # Створення потоку відео
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
