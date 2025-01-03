ca211ad087e5bd5a75c608607a717d2c73b93d26
taskcomplete <52295038+taskcomplete@users.noreply.github.com>
2025-01-03 10:24:23 +0200

    test which GSTREAMER

--- raspberry-stream.py
+++ raspberry-stream.py
@@ -1,14 +1,26 @@
+from flask import Flask, Response
 import cv2
 
-camera = cv2.VideoCapture(0)
-while True:
-    ret, frame = camera.read()
-    if not ret:
-        print("Cannot access the camera")
-        break
-    cv2.imshow('Camera Test', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+app = Flask(__name__)
 
-camera.release()
-cv2.destroyAllWindows()
+# Використання libcamera через GStreamer
+camera = cv2.VideoCapture('libcamerasrc ! video/x-raw,width=640,height=480,framerate=30/1 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
